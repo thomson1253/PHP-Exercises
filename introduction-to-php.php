9398f0ff84c275c5d3a97f9d781842a01c1ebf48
thomson1253 <59714127+thomson1253@users.noreply.github.com>
2020-01-14 22:33:35 -0500

    Add files via upload

---
+++ introduction-to-php.php
@@ -0,0 +1,26 @@
+<?php
+
+$txt1 = "Welcome to PHP!";
+print "<h1>" . $txt1 . "</h1>";
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+$txt2 = "PHP stands for Hypertext Preprocessor!";
+print "<h2>" . $txt2 . "</h2>";
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+print "<p>PHP stands for Hypertext Preprocessor!</p>";
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+
+echo nl2br("HTML - Stands for Hypertext Markup Language\nCSS - Stands for Cascading Stylesheet\nJS - Stands for JavaScript\nPHP - Stands for Hypertext Preprocessor\nSQL - Stands for Structural Query Language");
+
+
+?>
+
+ 
